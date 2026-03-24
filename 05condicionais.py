bb23c9cd9f6e99ae5f2eeabb428acdf9897bda8d
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-24 14:04:23 -0300

    sistema de triagem de emails - setor de UX

--- 05condicionais.py
+++ 05condicionais.py
@@ -1,43 +1,43 @@
 #01 - Validação de investimento
-# valor = input("Qual valor deseja investir(ex. R$5.000,00)?  ")
-# valor = float(valor.replace("R$", "").replace(".", "").replace(",", "."))
+valor = input("Qual valor deseja investir(ex. R$5.000,00)?  ")
+valor = float(valor.replace("R$", "").replace(".", "").replace(",", "."))
 
-# if valor < 0:
-#     print("valor deve ser maior que zero")
-# elif valor < 1000:
-#     print("Perfil iniciante: sugerimos tesouro direto")
-# elif valor >= 1000 and valor < 5000:
-#     print("Perfil moderado: sujerimos fundos imobiliários")
-# else:
-#     print("Perfil arrojado: seujerimos ações")
+if valor < 0:
+    print("valor deve ser maior que zero")
+elif valor < 1000:
+    print("Perfil iniciante: sugerimos tesouro direto")
+elif valor >= 1000 and valor < 5000:
+    print("Perfil moderado: sujerimos fundos imobiliários")
+else:
+    print("Perfil arrojado: seujerimos ações")
 
-#02 - Controle de acesso ao sistema
-# admins = ["ana@empresa.com", "rafael@empresa.com", "marcionic@empresa.com"]
-# email = input("Insira seu email: ")
-# email = email.lower().strip()
+# 02 - Controle de acesso ao sistema
+admins = ["ana@empresa.com", "rafael@empresa.com", "marcionic@empresa.com"]
+email = input("Insira seu email: ")
+email = email.lower().strip()
 
-# if email in admins:
-#     print("Parabéns, acesso permitido!")
-# else:
-#     print("Acesso negado!")
+if email in admins:
+    print("Parabéns, acesso permitido!")
+else:
+    print("Acesso negado!")
 
 #03 - Cálculo de desconto progressivo
-# valor_carrinho = input("Valor do carrinho: ")
-# valor_carrinho = float(valor_carrinho.replace("R$", "").replace(".", "").replace(",", "."))
-# if valor_carrinho >= 500:
-#     desconto = valor_carrinho * 0.15
-#     valor_carrinho = valor_carrinho - desconto
-#     print(f"Desconto: R${desconto:,.2f}")
-#     print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
-# elif valor_carrinho >= 200:
-#     desconto = valor_carrinho * 0.10
-#     valor_carrinho = valor_carrinho - desconto
-#     print(f"Desconto: R${desconto:,.2f}")
-#     print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
-# else:
-#     desconto = 0
-#     print(f"Desconto: R${desconto:,.2f}")
-#     print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
+valor_carrinho = input("Valor do carrinho: ")
+valor_carrinho = float(valor_carrinho.replace("R$", "").replace(".", "").replace(",", "."))
+if valor_carrinho >= 500:
+    desconto = valor_carrinho * 0.15
+    valor_carrinho = valor_carrinho - desconto
+    print(f"Desconto: R${desconto:,.2f}")
+    print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
+elif valor_carrinho >= 200:
+    desconto = valor_carrinho * 0.10
+    valor_carrinho = valor_carrinho - desconto
+    print(f"Desconto: R${desconto:,.2f}")
+    print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
+else:
+    desconto = 0
+    print(f"Desconto: R${desconto:,.2f}")
+    print(f"Valor do carrinho: R${valor_carrinho:,.2f}")
 
 #04 - Análise de metas combinadas 
 meta_vendedor = float(input("Meta vendedor: "))
@@ -51,3 +51,14 @@ else:
     bonus = 0
 
 print(f"Seu bônus é R${bonus:,.2f}")
+
+#05 - Sistema de triagem de e-mails
+assunto = input("Assunto email: ")
+assunto = assunto.lower()
+
+if "pagamento" or "boleto" in assunto:
+    print("Ecaminhado para o setor financeiro")
+elif "entrega" or "atraso" in assunto:
+    print("Encaminhado para o setor de logística")
+else:
+    print("Encaminhado para o setor de atendimento geral")
