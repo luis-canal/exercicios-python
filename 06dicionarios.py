a583629410135d1c6e4511a64016764399e043e7
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-26 14:01:27 -0300

    análise de faturamento por região - setor financeiro

--- 06dicionarios.py
+++ 06dicionarios.py
@@ -6,10 +6,22 @@
 # print(clientes)
 
 #02 - Consulta de estoque
-estoque = {"teclado": 50, "mouse": 30, "monitor": 45}
-produto = input("Digite o produto: ")
-produto = produto.lower().strip()
-if produto in estoque:
-    print(f"Produto: {produto}, Quantidade: {estoque[produto]}")
-else:
-    print("Produto não encontrado")
+# estoque = {"teclado": 50, "mouse": 30, "monitor": 45}
+# produto = input("Digite o produto: ")
+# produto = produto.lower().strip()
+# if produto in estoque:
+#     print(f"Produto: {produto}, Quantidade: {estoque[produto]}")
+# else:
+#     print("Produto não encontrado")
+
+#03 - Análise de faturamento por região
+vendas_regiao = {"Norte": 1500,
+                 "Sul": 4600,
+                 "Leste": 1300,
+                 "Oeste": 5500,}
+lista_vendas = list(vendas_regiao.values())
+total_valor = sum(lista_vendas)
+total_regioes = len(lista_vendas)
+media = total_valor / total_regioes
+print("Valor total de vendas: ", total_valor)
+print("Média de vendas entre as regiões: ", media)
