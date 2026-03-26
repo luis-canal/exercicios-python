236b68b008cfa1710e9d1bb05758f5161d21c94d
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-26 13:44:26 -0300

    Atualização de cadastro de clientes - setor de CRM

---
+++ 06dicionarios.py
@@ -0,0 +1,6 @@
+#01 Atualização de cadastro de clientes
+clientes = {"Lira": 5000, "Alon": 3000, "Julia": 4500}
+nova_compra = 1500
+clientes["Alon"] = clientes["Alon"] + nova_compra
+clientes["Marcos"] = 2000
+print(clientes)
