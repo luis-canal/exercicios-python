a4edc37a84d62409a0dfdb817dd8ae92453c51df
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-27 14:07:04 -0300

    cálculo de comissão progressiva - setor de vendas

--- 07repeticoes.py
+++ 07repeticoes.py
@@ -1,9 +1,21 @@
 #01 - Automação de convites
-for i in range(11):
-    falta = 10 - i
-    if falta > 1:
-        print(f"Faltam {falta} minutos para começar a reunião")
-    elif falta == 1:
-        print(f"Falta {falta} minuto para começar a reunião")
+# for i in range(11):
+#     falta = 10 - i
+#     if falta > 1:
+#         print(f"Faltam {falta} minutos para começar a reunião")
+#     elif falta == 1:
+#         print(f"Falta {falta} minuto para começar a reunião")
+#     else:
+#         print("Reunião começou!!")
+
+#02 - Cálculo de comissão progressiva
+vendas = [2000, 5000, 1000, 8000, 3000]
+comissao_total = 0
+for i in vendas:
+    if i > 4000:
+        comissao = 0.1
     else:
-        print("Reunião começou!!")
+        comissao = 0.05
+    comissao_total = comissao_total + i * comissao
+print("Lista de vendas: ", vendas)
+print("Comissão total: ", comissao_total)
