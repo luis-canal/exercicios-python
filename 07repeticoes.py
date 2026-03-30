f6ec0476d7a4a6c0c855727c7c67734c0b588ccd
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-30 14:53:52 -0300

    exercícios de laços de repetições completo

--- 07repeticoes.py
+++ 07repeticoes.py
@@ -1,50 +1,50 @@
 #01 - Automação de convites
-# for i in range(11):
-#     falta = 10 - i
-#     if falta > 1:
-#         print(f"Faltam {falta} minutos para começar a reunião")
-#     elif falta == 1:
-#         print(f"Falta {falta} minuto para começar a reunião")
-#     else:
-#         print("Reunião começou!!")
+for i in range(11):
+    falta = 10 - i
+    if falta > 1:
+        print(f"Faltam {falta} minutos para começar a reunião")
+    elif falta == 1:
+        print(f"Falta {falta} minuto para começar a reunião")
+    else:
+        print("Reunião começou!!")
 
 #02 - Cálculo de comissão progressiva
-# vendas = [2000, 5000, 1000, 8000, 3000]
-# comissao_total = 0
-# for i in vendas:
-#     if i > 4000:
-#         comissao = 0.1
-#     else:
-#         comissao = 0.05
-#     comissao_total = comissao_total + i * comissao
-# print("Lista de vendas: ", vendas)
-# print("Comissão total: ", comissao_total)
+vendas = [2000, 5000, 1000, 8000, 3000]
+comissao_total = 0
+for i in vendas:
+    if i > 4000:
+        comissao = 0.1
+    else:
+        comissao = 0.05
+    comissao_total = comissao_total + i * comissao
+print("Lista de vendas: ", vendas)
+print("Comissão total: ", comissao_total)
 
 #03 - Verificação de estoque crítico
-# estoque = ["monitor", "mouse", "teclado", "gabinete", "processador"]
-# quantidade = [10, 35, 4, 20, 7]
-# for i in quantidade: 
-#     if i < 8:
-#         posicao = quantidade.index(i)
-#         print(f"ALERTA: o produto {estoque[posicao]} está com apenas {i} itens")
+estoque = ["monitor", "mouse", "teclado", "gabinete", "processador"]
+quantidade = [10, 35, 4, 20, 7]
+for i in quantidade: 
+    if i < 8:
+        posicao = quantidade.index(i)
+        print(f"ALERTA: o produto {estoque[posicao]} está com apenas {i} itens")
 
 #04 - Análise de custos mensais - setor financeiro
-# metas = {
-#     "jan": 1000,
-#     "fev": 1500,
-#     "mar": 2000
-# }
-# gastos = {
-#     "jan": 859,
-#     "fev": 1576,
-#     "mar": 3190
-# }
-# for mes in gastos:
-#     if gastos[mes] > metas[mes]:
-#         diferenca = gastos[mes] - metas[mes]
-#         print(f"Mês {mes}: orçamento estourado em {diferenca} reais.")
-#     else:
-#         print(f"Mês {mes}: dentro do orçamento!!")
+metas = {
+    "jan": 1000,
+    "fev": 1500,
+    "mar": 2000
+}
+gastos = {
+    "jan": 859,
+    "fev": 1576,
+    "mar": 3190
+}
+for mes in gastos:
+    if gastos[mes] > metas[mes]:
+        diferenca = gastos[mes] - metas[mes]
+        print(f"Mês {mes}: orçamento estourado em {diferenca} reais.")
+    else:
+        print(f"Mês {mes}: dentro do orçamento!!")
 
 #05 - Reajuste geral dos preços - setor comercial
 precos = {"celular": 1500, "tablet": 1000, "notebook": 1250}
