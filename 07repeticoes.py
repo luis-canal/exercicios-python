de42a26e149cc4845b478e6fad52db41128d028a
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-30 14:27:43 -0300

    verificação de estoque crítico - setor de logística

--- 07repeticoes.py
+++ 07repeticoes.py
@@ -9,13 +9,21 @@
 #         print("Reunião começou!!")
 
 #02 - Cálculo de comissão progressiva
-vendas = [2000, 5000, 1000, 8000, 3000]
-comissao_total = 0
-for i in vendas:
-    if i > 4000:
-        comissao = 0.1
-    else:
-        comissao = 0.05
-    comissao_total = comissao_total + i * comissao
-print("Lista de vendas: ", vendas)
-print("Comissão total: ", comissao_total)
+# vendas = [2000, 5000, 1000, 8000, 3000]
+# comissao_total = 0
+# for i in vendas:
+#     if i > 4000:
+#         comissao = 0.1
+#     else:
+#         comissao = 0.05
+#     comissao_total = comissao_total + i * comissao
+# print("Lista de vendas: ", vendas)
+# print("Comissão total: ", comissao_total)
+
+#03 - Verificação de estoque crítico
+estoque = ["monitor", "mouse", "teclado", "gabinete", "processador"]
+quantidade = [10, 35, 4, 20, 7]
+for i in quantidade: 
+    if i < 8:
+        posicao = quantidade.index(i)
+        print(f"ALERTA: o produto {estoque[posicao]} está com apenas {i} itens")
