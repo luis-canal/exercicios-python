43243e558bd939fa85f3328f952677d40b9752c4
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-01 13:31:15 -0300

    Analisador de margem de lucro - setor financeiro

--- 08funcoes.py
+++ 08funcoes.py
@@ -9,14 +9,24 @@
 #     print(produto_padronizado)
 
 #02 - Calculadora de imposto sobre serviço - setor fiscal
-def calcular_iss(valor):
-    if valor > 5000:
-        taxa = 0.05
-    else:
-        taxa = 0.03
-    imposto = valor * taxa
-    print("O valor do imposto é: ", imposto)
-calcular_iss(8000)
-calcular_iss(2000)
+# def calcular_iss(valor):
+#     if valor > 5000:
+#         taxa = 0.05
+#     else:
+#         taxa = 0.03
+#     imposto = valor * taxa
+#     print("O valor do imposto é: ", imposto)
+# calcular_iss(8000)
+# calcular_iss(2000)
 
+#03 - Analisador de margem de lucro - setor financeiro
+def analisar_margem(fat, custo):
+    lucro = fat - custo
+    margem = lucro / fat
+    if margem >= 0.3:
+        print("Margem saudável")
+    else:
+        print("Margem baixa")
+
+analisar_margem(10000, 6000)
 
