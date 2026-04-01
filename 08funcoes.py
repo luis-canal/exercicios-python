0b57eb4b5ffc9472c11ac893860d39aa80d2afdb
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-01 13:26:48 -0300

    calculadora de imposto sobre o serviço - setor  fiscal

--- 08funcoes.py
+++ 08funcoes.py
@@ -1,9 +1,22 @@
 #01 - Padronizador de produtos -  setor de e-comerce
-produtos_baguncados = ["iphone 13", "AiRpOdS Pro", "iPad mini", "caixa de som bluetooth"]
-def padronizar_texto(texto):
-    texto = texto.strip().title()
-    return texto
+# produtos_baguncados = ["iphone 13", "AiRpOdS Pro", "iPad mini", "caixa de som bluetooth"]
+# def padronizar_texto(texto):
+#     texto = texto.strip().title()
+#     return texto
 
-for produto in produtos_baguncados:
-    produto_padronizado = padronizar_texto(produto)
-    print(produto_padronizado)
+# for produto in produtos_baguncados:
+#     produto_padronizado = padronizar_texto(produto)
+#     print(produto_padronizado)
+
+#02 - Calculadora de imposto sobre serviço - setor fiscal
+def calcular_iss(valor):
+    if valor > 5000:
+        taxa = 0.05
+    else:
+        taxa = 0.03
+    imposto = valor * taxa
+    print("O valor do imposto é: ", imposto)
+calcular_iss(8000)
+calcular_iss(2000)
+
+
