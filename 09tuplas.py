ee14d5e1c936f0aee134d42da8c36c580dfecdca
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-06 15:19:33 -0300

    Localização de entregas - setor de logística

---
+++ 09tuplas.py
@@ -0,0 +1,4 @@
+#01 - Localização de entregas - setor de logística
+coordenadas = (-25.9965, -54.7863)
+latitude, longitude = coordenadas #unpacking
+print(f"Latitude: {latitude} e Longitude: {longitude}")
