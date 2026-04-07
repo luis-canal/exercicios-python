b4ffb840bf224ae4d512899fb30d71b0b8198e45
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-07 11:04:46 -0300

    Resumo de folha de pagamento - setor de RH

--- 09tuplas.py
+++ 09tuplas.py
@@ -1,4 +1,14 @@
 #01 - Localização de entregas - setor de logística
-coordenadas = (-25.9965, -54.7863)
-latitude, longitude = coordenadas #unpacking
-print(f"Latitude: {latitude} e Longitude: {longitude}")
+# coordenadas = (-25.9965, -54.7863)
+# latitude, longitude = coordenadas #unpacking
+# print(f"Latitude: {latitude} e Longitude: {longitude}")
+
+#02 - Resumo de folha de pagamento - setor de RH
+def  calcular_folha(salario):
+    perc_desconto = 0.1
+    desconto = perc_desconto * salario
+    salario_liq = salario - desconto
+    return desconto, salario_liq
+
+desconto, salario_liquido = calcular_folha(10000)
+print(f"Desconto: {desconto} | Salário liquído: {salario_liquido}")
