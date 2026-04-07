3c149d16b3c7c0d4a6c241d97b724d1a83ab256b
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-07 14:45:51 -0300

    Processamento de vendas regionais - setor de dashboard

--- 09tuplas.py
+++ 09tuplas.py
@@ -14,9 +14,24 @@
 # print(f"Desconto: {desconto} | Salário liquído: {salario_liquido}")
 
 #03 - Processamento de vendas por unidade - setor comercial
-vendas_dia = [("monitor", 900, 25), ("teclado", 150, 40), ("mouse", 90, 36)]
-for i in vendas_dia:
-    produto = i[0]
-    preco = i[1]
-    qtde = i[2]
-    print(f"Produto: {produto} | Preço: {preco} | Quantidade: {qtde} | Total: {preco * qtde}")
+# vendas_dia = [("monitor", 900, 25), ("teclado", 150, 40), ("mouse", 90, 36)]
+# for i in vendas_dia:
+#     produto = i[0]
+#     preco = i[1]
+#     qtde = i[2]
+#     print(f"Produto: {produto} | Preço: {preco} | Quantidade: {qtde} | Total: {preco * qtde}")
+
+#04 - Processamento de vendas regionais - setor de dashboard
+dados_filias = {"Matriz": [10000, 20000, 15000],
+                "Filial Norte": [7000, 3000, 19000],
+                "Filial Sul": [13000, 20000],}
+
+def analisar_vendas(lista_vendas):
+    total_vendas = sum(lista_vendas)
+    media_vendas = total_vendas / len(lista_vendas)
+    return total_vendas, media_vendas
+
+for f in dados_filias:
+    vendas_filial = dados_filias[f]
+    total_vendas_filial, media_vendas_filial = analisar_vendas(vendas_filial)
+    print(f"Filial -> {f}: Total de vendas: {total_vendas_filial} | Media de vendas: {media_vendas_filial}")
