0c8be5121b974373cd66a44c5bb628136ed09695
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-07 11:11:47 -0300

    Processamento de vendas por unidade - setor comercial

--- 09tuplas.py
+++ 09tuplas.py
@@ -4,11 +4,19 @@
 # print(f"Latitude: {latitude} e Longitude: {longitude}")
 
 #02 - Resumo de folha de pagamento - setor de RH
-def  calcular_folha(salario):
-    perc_desconto = 0.1
-    desconto = perc_desconto * salario
-    salario_liq = salario - desconto
-    return desconto, salario_liq
+# def  calcular_folha(salario):
+#     perc_desconto = 0.1
+#     desconto = perc_desconto * salario
+#     salario_liq = salario - desconto
+#     return desconto, salario_liq
 
-desconto, salario_liquido = calcular_folha(10000)
-print(f"Desconto: {desconto} | Salário liquído: {salario_liquido}")
+# desconto, salario_liquido = calcular_folha(10000)
+# print(f"Desconto: {desconto} | Salário liquído: {salario_liquido}")
+
+#03 - Processamento de vendas por unidade - setor comercial
+vendas_dia = [("monitor", 900, 25), ("teclado", 150, 40), ("mouse", 90, 36)]
+for i in vendas_dia:
+    produto = i[0]
+    preco = i[1]
+    qtde = i[2]
+    print(f"Produto: {produto} | Preço: {preco} | Quantidade: {qtde} | Total: {preco * qtde}")
